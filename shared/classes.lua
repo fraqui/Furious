332a09bbdefa58435e78362b6a551703634c1b08
Fraqui <fraqui7ytb0@gmail.com>
2026-09-14 23:10:01 +0200

    Update classes.lua

--- shared/classes.lua
+++ shared/classes.lua
@@ -1,6 +1,19 @@
 Player = {}
 Player.__index = Player
 
+---@class Player
+---@field source number
+---@field name string
+---@field ip string
+---@field license string
+---@field discord string
+---@field permid number
+---@field characterId number
+---@field loaded boolean
+---@field group string
+
+---@param data table
+---@return Player
 function Player:New(data)
     data = data or {}
 
@@ -16,9 +29,59 @@ function Player:New(data)
 
     self.permid = data.permid
 
-    self.character = data.character
+    self.characterId = data.characterId
 
     self.loaded = false
+
+    self.group = data.group or "user"
+
+
+    function self:Load()
+        self.loaded = true
+    end
+
+    ---@return string
+    function self:GetName()
+        return self.name
+    end
+
+    ---@return string
+    function self:GetGroup()
+        return self.group
+    end
+
+    ---@return number
+    function self:GetId()
+        return self.permid
+    end
+
+    ---@return string
+    function self:GetLicense()
+        return self.license
+    end
+
+    ---@return string
+    function self:GetDiscord()
+        return self.discord
+    end
+
+    ---@param reason string
+    function self:Kick(reason)
+        DropPlayer(self.source, reason)
+    end
+
+    ---@param new_group string
+    function self:SetGroup(new_group)
+        local last_group = self.group
+
+        ExecuteCommand("remove_principal identifier.license:" .. self.license .. " group." .. last_group)
+
+        self.group = new_group
+
+        --Systeme de logs
+
+        ExecuteCommand("add_principal identifier.license:" .. self.license .. " group." .. new_group)
+    end
 
     return self
 end
